f9ac61625cca98b6aed1ecab247e32c5633cba2c
Sanaz <snz5seifi@gmail.com>
2022-05-21 12:16:25 -0400

    Use Method simply

--- src/main/java/com/example/learn_java/Learn_Java.java
+++ src/main/java/com/example/learn_java/Learn_Java.java
@@ -1,4 +1,13 @@
 package com.example.learn_java;
 
 public class Learn_Java {
+
+    public static void main(String[] args){
+        greetUser("Sanaz");
+    }
+
+    public static void greetUser(String name){
+        System.out.println("Hello " + name);
+
+    }
 }
