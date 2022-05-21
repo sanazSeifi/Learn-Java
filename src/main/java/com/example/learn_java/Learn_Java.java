a60a38df784d9310d9fc81817ee71f8fc9de6519
Sanaz <snz5seifi@gmail.com>
2022-05-21 12:41:44 -0400

    change Viod method to a String Method

--- src/main/java/com/example/learn_java/Learn_Java.java
+++ src/main/java/com/example/learn_java/Learn_Java.java
@@ -3,12 +3,13 @@ package com.example.learn_java;
 public class Learn_Java {
 
     public static void main(String[] args){
-        greetUser("Sanaz" , "Seifi");
+        String massage = greetUser("Sanaz" , "Seifi");
+        System.out.println(massage);
 
     }
 
-    public static void greetUser(String name, String lastName){
-        System.out.println("Hello " + name + " "+ lastName);
+    public static String greetUser(String name, String lastName){
+        return  "Hello " + name + " "+ lastName;
 
     }
 }
