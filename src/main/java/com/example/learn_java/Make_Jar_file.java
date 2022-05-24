b9938649c62e2cd1fa6fc31cf3a8744c24c37cdd
Sanaz <snz5seifi@gmail.com>
2022-05-24 18:16:49 -0400

    making Jar file

--- src/main/java/com/example/learn_java/Make_Jar_file.java
+++ src/main/java/com/example/learn_java/Make_Jar_file.java
@@ -2,6 +2,6 @@ package com.example.learn_java;
 
 public class Make_Jar_file {
     public static void main(String[] args){
-        System.out.println("Let's Make Jar File :)");
+        System.out.println("Let's Make Jar File :)  ");
     }
 }
