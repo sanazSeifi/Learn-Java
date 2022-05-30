8b27c5991facc75547ea6825c6b39ff4b454c840
Sanaz <snz5seifi@gmail.com>
2022-05-30 17:58:16 -0400

    Adding sendHttpRequest and findIpAddress

--- src/main/java/com/example/learn_java/Browser.java
+++ src/main/java/com/example/learn_java/Browser.java
@@ -1,4 +1,19 @@
 package com.example.learn_java;
 
+import java.security.PublicKey;
+
 public class Browser {
+    public void navigate(String address){
+        String ip = findIPAddress(address);
+        String html = sendHttpRequest(ip);
+        System.out.println(html);
+    }
+
+    public String sendHttpRequest(String ip) {
+        return "<html></html>";
+    }
+
+    public String findIPAddress(String address){
+    return "127.83";
+    }
 }
