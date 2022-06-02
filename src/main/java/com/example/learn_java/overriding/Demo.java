b4248d12c062001108aa2979cde31208ed65ceff
Sanaz <snz5seifi@gmail.com>
2022-06-02 01:55:05 -0400

    we can override the display method fromperson class and use that in Student class

--- src/main/java/com/example/learn_java/overriding/Demo.java
+++ src/main/java/com/example/learn_java/overriding/Demo.java
@@ -1,4 +1,11 @@
 package com.example.learn_java.overriding;
 
 public class Demo {
+    public static void main(String[] args){
+        Person p = new Person("Bob", 21);
+        Student s = new Student("Mary", 19, 34762);
+        System.out.println();
+        p.display();
+        s.display();
+    }
 }
--- src/main/java/com/example/learn_java/overriding/Person.java
+++ src/main/java/com/example/learn_java/overriding/Person.java
@@ -1,4 +1,20 @@
 package com.example.learn_java.overriding;
 
 public class Person {
+    public String name;
+    public int age;
+
+    public void display(){
+        System.out.println(name + " is " + age + " years old");
+    }
+
+    public Person(){
+        name = "Unknown name";
+        age =0;
+    }
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }
+
