159edc1382b06fc83c324d300b46c6c1e9c4fff2
Sanaz <snz5seifi@gmail.com>
2022-06-02 00:54:20 -0400

    Super and constructor

--- src/main/java/com/example/learn_java/MainSuper.java
+++ src/main/java/com/example/learn_java/MainSuper.java
@@ -3,7 +3,6 @@ package com.example.learn_java;
 public class MainSuper {
     public static void main(String[] args){
         Car c = new Car();
-        c.Vroom();
 
     }
 }
--- src/main/java/com/example/learn_java/WhatsSuper.java
+++ src/main/java/com/example/learn_java/WhatsSuper.java
@@ -1,14 +1,11 @@
 package com.example.learn_java;
 
  class WhatsSuper{
-    public void Vroom(){
-        System.out.println("Vroom Vroom");
 
-    }
  }
 class Car extends WhatsSuper{
-     public void Vroom(){
-         super.Vroom();
+     Car(){
+
      }
 
 }
