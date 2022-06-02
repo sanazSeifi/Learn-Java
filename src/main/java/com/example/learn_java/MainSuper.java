e9648aca1d225a707072f054ce1d692b1750fe10
Sanaz <snz5seifi@gmail.com>
2022-06-02 00:59:58 -0400

    constructor with and without Parameters

--- src/main/java/com/example/learn_java/MainSuper.java
+++ src/main/java/com/example/learn_java/MainSuper.java
@@ -3,6 +3,7 @@ package com.example.learn_java;
 public class MainSuper {
     public static void main(String[] args){
         Car c = new Car();
+        System.out.println("Default constructor");
 
     }
 }
--- src/main/java/com/example/learn_java/WhatsSuper.java
+++ src/main/java/com/example/learn_java/WhatsSuper.java
@@ -1,11 +1,15 @@
 package com.example.learn_java;
 
  class WhatsSuper{
-
+     WhatsSuper(){
+         System.out.println("WhatsSuper constructor");
+     }
  }
 class Car extends WhatsSuper{
-     Car(){
-
+     int maxSpeed;
+     Car(int maxSpeed){
+         System.out.println("Car Constructor");
+         this.maxSpeed =maxSpeed;
      }
 
 }
