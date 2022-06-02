51a12f2196d240823016e78576d9bb5daacd2665
Sanaz <snz5seifi@gmail.com>
2022-06-01 20:22:47 -0400

    There was a problem!!

---
+++ src/main/java/com/example/learn_java/MainSuper.java
@@ -0,0 +1,11 @@
+package com.example.learn_java;
+
+public class MainSuper {
+    public static void main(String[] args){
+        WhatsSuper c = new WhatsSuper();
+        int maxSpeed = c.maxSpeed;
+        System.out.println(maxSpeed);
+        c.display();
+
+    }
+}
--- src/main/java/com/example/learn_java/WhatsSuper.java
+++ src/main/java/com/example/learn_java/WhatsSuper.java
@@ -1,4 +1,13 @@
 package com.example.learn_java;
 
-public class WhatsSuper {
+ class WhatsSuper{
+    int maxSpeed =120;
+
+     public void display() {
+     }
+
+     class Car extends WhatsSuper{
+        int maxSpeed = 100;
+
+    }
 }
