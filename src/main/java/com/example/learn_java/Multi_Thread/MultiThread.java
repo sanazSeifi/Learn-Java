e257e0175c73be27b84d131e0e741813b85dbcbc
Sanaz <snz5seifi@gmail.com>
2022-06-06 11:53:09 -0400

    Problem? hy there is no different between run and start method?

--- src/main/java/com/example/learn_java/Multi_Thread/MultiThread.java
+++ src/main/java/com/example/learn_java/Multi_Thread/MultiThread.java
@@ -3,6 +3,8 @@ package com.example.learn_java.Multi_Thread;
 public class MultiThread {
 public static void main(String[] args){
     MultiThreadThings myThing = new MultiThreadThings();
-    myThing.start();
+    MultiThreadThings myThing2 = new MultiThreadThings();
+    myThing.run();
+    myThing2.run();
 }
 }
