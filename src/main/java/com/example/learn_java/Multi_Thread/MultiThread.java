d11f91f7aaad540c92f00fe2373cc47fbe59b5f6
Sanaz <snz5seifi@gmail.com>
2022-06-06 12:36:38 -0400

    Print thread number

--- src/main/java/com/example/learn_java/Multi_Thread/MultiThread.java
+++ src/main/java/com/example/learn_java/Multi_Thread/MultiThread.java
@@ -2,9 +2,10 @@ package com.example.learn_java.Multi_Thread;
 
 public class MultiThread {
 public static void main(String[] args){
-    MultiThreadThings myThing = new MultiThreadThings();
-    MultiThreadThings myThing2 = new MultiThreadThings();
-    myThing.run();
-    myThing2.run();
+
+    for(int i = 0; i<= 5; i++) {
+        MultiThreadThings myThing = new MultiThreadThings(i);
+        myThing.start();
+    }
 }
 }
